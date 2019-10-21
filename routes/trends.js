057aab58ef33bcdcc6ef3d4aa970e6eb5dfd8186
Rafael Montenegro <rafael@mesainc.com.br>
2019-10-20 23:17:58 -0700

    Get trends endpoint

--- routes/trends.js
+++ routes/trends.js
@@ -2,46 +2,106 @@ const express = require("express");
 const router = express.Router();
 
 const googleTrends = require('google-trends-api');
-
 const { getComparisonArrays, getValuesInProportion } = require('./trendsUtils.js')
+const { getLanguages, getLanguagesLatestCount } = require('../database/databaseUtils.js')
 
-const { getLanguages } = require('../database/databaseUtils.js')
+//TODO - Redo searches for CA (atm just US is being checked)
+//TODO - Cache trend search result for reuse.
+//TODO - Refactor hardcoded date for trends
 
 router.get('/trends', (req, res) => {
 
+    let languageGlobal;
 
-    getLanguages()
-        .then(r => {
-            console.log(r);
-            return getComparisonArrays(r)
+    getLanguagesLatestCount()
+        // getLanguages()
+        .then(langs => {
+            languageGlobal = langs
+
+            return start(languageGlobal)
         })
+        .then(trends => {
 
-        .then(r => {
-            console.log(r);
+            if (trends.usa[0].name === languageGlobal[0].name) {
 
-            return getAllTrends(r)
+                //Response #1
+                res.send(trends)
+
+            } else {
+
+                //....................//....................//....................
+                // If the base programming language languageGlobal[0] was NOT
+                // The most popular, the update the order of the languages,
+                // And search again.
+                //....................//....................//....................
+
+                let fakeFirstPlace = languageGlobal[0]
+
+                let i = 0
+                languageGlobal[0] = languageGlobal.find((item, index) => {
+                    i = index
+                    console.log(i);
+
+                    return item.name === trends.usa[0].name
+                })
+
+                languageGlobal[i] = fakeFirstPlace
+
+                //Search again:
+                start(languageGlobal)
+                    .then(r => {
+
+                        //Response #2
+                        res.send(r)
+                    })
+            }
+            //....................//....................//....................
+
         })
-
-        .then(arrayOfTrends => {
-
-            console.log('done..a!!');
-            console.log(arrayOfTrends);
-            
-        })
-
-
-
-
 })
 
 module.exports.trendsRouter = router;
 
+const start = (languages) => {
+    return new Promise((resolve, reject) => {
 
-/////////////////////////////////////////////////////////////////////////////////
+        languages.forEach(item => {
+            delete item.total
 
-let resultArray = []
+            //todo - remove this
+            // delete item.id_language
+            // delete item.description
+        })
 
-const getAllTrends = (array) => {
+        console.log(languages);
+
+        /** Another promise chain - RESULT HERE */
+
+        getComparisonArrays(languages)
+            .then(arrayComparisons => {
+
+                console.log(arrayComparisons);
+
+                getAllTrends([...arrayComparisons])
+                    .then(arrayOfTrends => {
+
+                        const trends = getProportionObj([...arrayOfTrends], languages)
+
+                        resolve(trends)
+                    })
+            }) /** End final inner chain */
+
+
+    })//end promise
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Google Trends API requests:
+/////////////////////////////////////////////////////////////////////////////
+
+const getAllTrends = (array, resultArray = []) => {
+
+    const f = array[0]
 
     if (array.length > 0) {
 
@@ -59,11 +119,12 @@ const getAllTrends = (array) => {
 
                 resultArray.push(r)
 
-                return getAllTrends(array)
+                return getAllTrends(array, resultArray)
             })
 
     } else {
         return new Promise((resolve, reject) => {
+
             resolve(resultArray)
         })
     }
@@ -93,6 +154,7 @@ const getTrendLanguages = (languagesArray) => {
                 })
 
                 let popularityValues = popularityByLocation.map(item => {
+
                     return item.value
                 })
 
@@ -103,4 +165,71 @@ const getTrendLanguages = (languagesArray) => {
             })
 
     })
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Utils:
+///////////////////////////////////////////////////////////////////////////////
+
+const getProportionObj = (arrayOfTrends, languages) => {
+
+    //Split into countries:
+    let usa = []
+    let can = []
+
+    arrayOfTrends.forEach(item => {
+        usa.push(item[0])
+        can.push(item[1])
+    })
+
+    //Get proportion values:
+    usa = getValuesInProportion(usa)
+    can = getValuesInProportion(can)
+
+    //Now merge each array:
+    usa = mergeTrends(usa)
+    can = mergeTrends(can)
+
+    //Now map languages back to trends:
+    const m = (item, index) => {
+        return {
+            ...languages[index],
+            trend: item
+        }
+    }
+
+    usa = usa.map(m)
+    can = can.map(m)
+
+    //Sort both arrays:
+    const s = (a, b) => {
+        return b.trend - a.trend
+    }
+
+    usa = usa.sort(s)
+    can = can.sort(s)
+
+
+    const mapRanking = (item, index) => {
+        item.trend = index + 1
+        return item
+    }
+
+    return {
+        usa: usa.map(mapRanking),
+        can: can.map(mapRanking)
+    }
+}
+
+const mergeTrends = (trends) => {
+    let merged = [trends[0][0]] //Starts with the value of the first
+
+    //Fills the rest ignoring first item:
+    trends.forEach(item => {
+        for (let index = 1; index < item.length; index++) {
+            merged.push(item[index]);
+        }
+    })
+
+    return merged
 }
--- routes/trendsUtils.js
+++ routes/trendsUtils.js
@@ -1,12 +1,12 @@
 
-const getSplitArrays = (languages) => {
+const getSplitArrays = (languages, size = 4) => {
 
     let arraysForComparison = [[]]
     let count = 0
 
     languages.forEach(item => {
 
-        if (count != 4) {
+        if (count != size) {
 
             arraysForComparison[arraysForComparison.length - 1].push(item)
 
@@ -46,6 +46,7 @@ const getFakeComparison = () => {
  * Returns a value that is a proportion to the "p".
  */
 const getValueBasedPis = (p, otherValue) => {
+
     let vP = otherValue / p
     return vP
 }
@@ -86,29 +87,84 @@ module.exports.getFakeDataTrends = getFakeDataTrends
  * value -> Is a float based like, 0.7727272727272727
  */
 const getValuesInProportion = (array) => {
-    return new Promise((resolve, reject) => {
 
-        const finalArrayOfArrays = []
+    // let total_highestRanking = (array.length * array[0].length) - array.length
 
-        array.forEach(itemArray => {
+    const finalArrayOfArrays = []
 
-            const newArray = itemArray.map((item, index) => {
-                if (index == 0) {
-                    return 1
-                } else {
-                    return getValueBasedPis(itemArray[0], item)
-                }
-            })
+    //Gives a ranking for each individual array items:
+    array.forEach(itemArray => {
+        finalArrayOfArrays.push(getRankedArray(itemArray))
+    })
 
-            finalArrayOfArrays.push(newArray)
-        })
+    //Now gives ranking for all items in all arrays:
+    console.log('---overall ranking: -----');
+    console.log(finalArrayOfArrays);
 
-        resolve(finalArrayOfArrays)
 
-    })//End promise
+    return getValuesInProportionX(finalArrayOfArrays)
+
 }
 
-module.exports.getValuesInProportion = getValuesInProportion
+const getRankedArray = (array) => {
+    let maxRankInner = array.length
+    let rankArray = []
+
+    array.forEach(_ => {
+
+        let highestIndex = getIndexHighest(array)
+
+        //By now we have the highest index.
+        rankArray[highestIndex] = maxRankInner--
+        array[highestIndex] = -1
+    })
+
+    return rankArray
+}
+
+const getIndexHighest = (array) => {
+
+    //Gets first available index:
+    array.forEach((item, index) => {
+        if (item > 0) {
+            highestIndex = index
+        }
+    })
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+        if (element > array[highestIndex]) {
+            highestIndex = index
+        }
+    }
+
+    return highestIndex
+}
+
+const getValuesInProportionX = (array) => {
+
+    const finalArrayOfArrays = []
+
+    array.forEach(itemArray => {
+
+        const newArray = itemArray.map((item, index) => {
+            if (index == 0) {
+                return 1
+            } else {
+                return getValueBasedPis(itemArray[0], item)
+            }
+        })
+
+        finalArrayOfArrays.push(newArray)
+    })
+
+    return finalArrayOfArrays
+
+}
+
+module.exports.getValuesInProportion = getValuesInProportionX
+
 
 /**
  * Receives an array:
