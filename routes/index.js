d0fb85cbba1e3f321f15cddea68fe5a6b0f02790
Juan <50636966+juanfernandotirado@users.noreply.github.com>
2019-11-12 11:54:41 -0800

    Event endpoint created

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const {comparisonRouter} = require("./comparison.js")
 const {trendsRouter} = require("./trends.js")
 const {quotesRouter} = require("./quotes.js")
 const {userRouter} = require("./user.js")
+const {eventsRouter} = require("./events.js")
 
 router.use('/comparison', comparisonRouter)
 router.use('/comparison', trendsRouter)
 router.use('/quotes', quotesRouter)
 router.use('/user', userRouter)
+router.use('/events', eventsRouter)
 
 exports.routerIndex = router;
