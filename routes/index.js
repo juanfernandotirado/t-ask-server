945c54e12dd870b9a87abf740d4437456d8290c7
Rafael Montenegro <rafael@mesainc.com.br>
2019-12-04 16:42:02 -0800

    Removed old trends route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const {trendsRouter} = require("./trends.js")
 const {quotesRouter} = require("./quotes.js")
 const {userRouter} = require("./user.js")
 const {eventsRouter} = require("./events.js")
--- routes/trends.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {trends} = require('../controllers/trendController.js')
-
-
-router.get('/trends', trends)
-
-module.exports.trendsRouter = router;
